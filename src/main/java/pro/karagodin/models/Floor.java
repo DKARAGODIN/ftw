be5c2c9a682bc7fb0c9d3f7c8474b11dc30661d1
OldKrab <4shushakov@gmail.com>
2023-04-22 15:43:50 +0300

    add item to floor

--- src/main/java/pro/karagodin/models/Floor.java
+++ src/main/java/pro/karagodin/models/Floor.java
@@ -1,5 +1,20 @@
 package pro.karagodin.models;
 
 public class Floor {
+    protected Item item;
 
+    public Item getItem() {
+        return item;
+    }
+
+    /** remove item from floor and return it */
+    public Item pickItem() {
+        var pickedItem = this.item;
+        this.item = null;
+        return pickedItem;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
 }
