6b914110d8861a51b664d0a677931e762498f39d
Dmitrii.Karagodin <4319788@gmail.com>
2023-04-22 13:11:25 +0300

    Base Gui layout

--- src/main/java/pro/karagodin/Main.java
+++ src/main/java/pro/karagodin/Main.java
@@ -1,77 +1,71 @@
 package pro.karagodin;
 
-import com.googlecode.lanterna.SGR;
-import com.googlecode.lanterna.Symbols;
-import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.gui2.BasicWindow;
-import com.googlecode.lanterna.gui2.Button;
-import com.googlecode.lanterna.gui2.ComboBox;
-import com.googlecode.lanterna.gui2.DefaultWindowManager;
-import com.googlecode.lanterna.gui2.Direction;
-import com.googlecode.lanterna.gui2.EmptySpace;
-import com.googlecode.lanterna.gui2.GridLayout;
-import com.googlecode.lanterna.gui2.Label;
-import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
-import com.googlecode.lanterna.gui2.Panel;
-import com.googlecode.lanterna.gui2.Separator;
-import com.googlecode.lanterna.gui2.TextBox;
-import com.googlecode.lanterna.gui2.Window;
-import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
-import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
-import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
 import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.swing.SwingTerminal;
-import com.googlecode.lanterna.terminal.swing.SwingTerminalFrame;
 
-import javax.swing.*;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.TimeZone;
 
 
 public class Main {
+
+    private static final int MAX_COL = 239;
+    private static final int MAX_ROW = 67;
+    private static final int GUI_VERTICAL_LINE_COL = MAX_COL - 25;
+
+    private static int hero_col = MAX_COL / 2;
+    private static int hero_row = MAX_ROW / 2;
+    private static final TextCharacter HERO_CHAR = TextCharacter.fromCharacter('@', TextColor.ANSI.RED, TextColor.ANSI.GREEN)[0];
+    private static final TextCharacter BLACK_CHAR = TextCharacter.fromCharacter(' ', TextColor.ANSI.BLACK, TextColor.ANSI.BLACK)[0];
+    private static final TextCharacter VERTICAL_RED_LINE_CHAR = TextCharacter.fromCharacter('|', TextColor.ANSI.RED, TextColor.ANSI.BLACK)[0];
+
+
     public static void main(String[] args) throws IOException {
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
         try (Screen screen = terminalFactory.createScreen()) {
             screen.startScreen();
-            int col = 10;
-            int row = 10;
-            TextCharacter c = TextCharacter.fromCharacter('!', TextColor.ANSI.RED, TextColor.ANSI.GREEN)[0];
-            screen.setCharacter(col, row, c);
+            printWelcomeMessage(screen);
 
-            while (true) {
-                if (false) break;
+            printGUI(screen);
 
-                screen.doResizeIfNecessary();
 
-                screen.clear();
+            screen.setCharacter(hero_col, hero_row, HERO_CHAR);
+            screen.refresh(Screen.RefreshType.COMPLETE);
 
+
+screenLoop: while (true) {
                 KeyStroke key = screen.pollInput();
                 if (key != null) {
                     switch (key.getKeyType()) {
-                        case ArrowLeft -> col--;
-                        case ArrowRight -> col++;
-                        case ArrowDown -> row++;
-                        case ArrowUp -> row--;
+                        case ArrowLeft -> {
+                            screen.setCharacter(hero_col, hero_row, BLACK_CHAR);
+                            hero_col--;
+                        }
+                        case ArrowRight -> {
+                            screen.setCharacter(hero_col, hero_row, BLACK_CHAR);
+                            hero_col++;
+                        }
+                        case ArrowDown -> {
+                            screen.setCharacter(hero_col, hero_row, BLACK_CHAR);
+                            hero_row++;
+                        }
+                        case ArrowUp -> {
+                            screen.setCharacter(hero_col, hero_row, BLACK_CHAR);
+                            hero_row--;
+                        }
+                        case Character -> {
+                            if (key.getCharacter() == 'q')
+                                break screenLoop;
+                        }
                     }
+                    screen.setCharacter(hero_col, hero_row, HERO_CHAR);
+                    screen.refresh(Screen.RefreshType.DELTA);
                 }
-
-                screen.setCharacter(col, row, c);
-                screen.refresh();
-                Thread.sleep(5);
+                Thread.sleep(10);
             }
 
             screen.stopScreen();
@@ -79,5 +73,24 @@ public class Main {
         catch(IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void printGUI(Screen screen) {
+        for (int i = 0; i <= MAX_ROW; i++) {
+            screen.setCharacter(GUI_VERTICAL_LINE_COL, i, VERTICAL_RED_LINE_CHAR);
+        }
+    }
+
+    private static void printWelcomeMessage(Screen screen) throws InterruptedException, IOException {
+        TextGraphics textGraphics = screen.newTextGraphics();
+        textGraphics.putString(15,11, "Please maximise the window to go FOR THE WIN!");
+        screen.refresh();
+        while (true) {
+            TerminalSize newTerminalSize = screen.doResizeIfNecessary();
+            if (newTerminalSize != null && newTerminalSize.getColumns() == MAX_COL + 1 && newTerminalSize.getRows() == MAX_ROW+1)
+                break;
+            Thread.sleep(50);
+        }
+        screen.clear();
     }
 }
