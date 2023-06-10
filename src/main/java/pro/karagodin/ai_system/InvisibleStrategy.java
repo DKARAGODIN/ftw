7b9e26baf8de5cc87b93e3e4e0c923e4f6fd4a4f
SergeyKuz1001 <kuzivanov.ser@gmail.com>
2023-06-10 22:07:11 +0300

    Fix maven's errors

--- src/main/java/pro/karagodin/ai_system/InvisibleStrategy.java
+++ src/main/java/pro/karagodin/ai_system/InvisibleStrategy.java
@@ -1,11 +1,10 @@
 package pro.karagodin.ai_system;
 
+import java.io.IOException;
+
 import com.googlecode.lanterna.TextColor;
-import pro.karagodin.game_engine.Coordinate;
 import pro.karagodin.game_engine.MobWithPosition;
 import pro.karagodin.models.Map;
-
-import java.io.IOException;
 
 public class InvisibleStrategy implements Strategy {
 
